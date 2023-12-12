986bb860718261ab5e0c048f90511bf4bda9a86c
Matias Lopez <matias.lopez@rocketinsights.com>
2023-12-12 11:17:41 -0300

    Day 01 complete

--- Day01/day01.cpp
+++ Day01/day01.cpp
@@ -1,20 +1,37 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <vector>
-
-using namespace std;
+#include <utility>
 
 class Helper
 {
 public:
+   static const int VALIDDIGITS_SIZE = 20;
+   std::pair<std::string, int> validDigits[VALIDDIGITS_SIZE] = {
+      {"0", 0},
+      {"1", 1},
+      {"2", 2},
+      {"3", 3},
+      {"4", 4},
+      {"5", 5},
+      {"6", 6},
+      {"7", 7},
+      {"8", 8},
+      {"9", 9},
+      {"zero", 0},
+      {"one", 1},
+      {"two", 2},
+      {"three", 3},
+      {"four", 4},
+      {"five", 5},
+      {"six", 6},
+      {"seven", 7},
+      {"eight", 8},
+      {"nine", 9}
+   };
+
    Helper() {}
    ~Helper() {}
-
-   bool isNumber(const char character)
-   {
-      return (character>='0' && character<='9');
-   }
 
    int getLineValue(std::string line)
    {
@@ -22,28 +39,43 @@ public:
 
       int currentValue = 0;
       // find first number
+      int currentMatchValue = 0;
       for(int i=0; i<lineLength; i++)
       {
-         const char currentChar = line[i];
-         if( this->isNumber(currentChar) )
+         std::string::size_type lowestMatchPosition = lineLength;
+         // compare all the keys of the hash to find the one has the lower position
+         for(int j=0; j<VALIDDIGITS_SIZE; j++)
          {
-//         std::cout << currentChar << " " << currentChar-'0' << " ";
-            currentValue = 10 * (currentChar-'0');
-            break;
+            std::string key = validDigits[j].first;
+            std::string::size_type matchPosition = line.find(key);
+//         std::cout << "Looking for " << key << " on line " << line << std::endl;
+            if((matchPosition != std::string::npos) && (matchPosition < lowestMatchPosition))
+            {
+               lowestMatchPosition = matchPosition;
+               currentMatchValue = validDigits[j].second;
+//         std::cout << "Match " << key << " on line " << line << " - " << currentMatchValue << std::endl;
+            }
          }
       }
+      currentValue = currentMatchValue * 10;
 
       // find last number
-      for(int i=lineLength-1; i>=0; i--)
+      for(int i=0; i<lineLength; i++)
       {
-         const char currentChar = line[i];
-         if( this->isNumber(currentChar) )
+         std::string::size_type highestMatchPosition = 0;
+         // compare all the keys of the hash to find the one has the lower position
+         for(int j=VALIDDIGITS_SIZE-1; j>=0; j--)
          {
-//         std::cout << currentChar << " " << currentChar-'0' << " - ";
-            currentValue += (currentChar-'0');
-            break;
+            std::string key = validDigits[j].first;
+            std::string::size_type matchPosition = line.rfind(key);
+            if((matchPosition != std::string::npos) && (matchPosition > highestMatchPosition))
+            {
+               highestMatchPosition = matchPosition;
+               currentMatchValue = validDigits[j].second;
+            }
          }
       }
+      currentValue += currentMatchValue;
 
 //      std::cout << currentValue << " - " << line << std::endl;
       return currentValue;
