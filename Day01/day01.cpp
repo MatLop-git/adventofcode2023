cc139537069144c8fe5f183b07e4465b8b4fc873
Matias Lopez <matias.lopez@rocketinsights.com>
2023-12-11 21:25:00 -0300

    First puzzle

--- Day01/day01.cpp
+++ Day01/day01.cpp
@@ -1,10 +1,53 @@
+#include <fstream>
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
    std::cout << "Advent of Code 2023 - Day 01" << std::endl;
+
+   int totalSum = 0;
+   std::string currentLine;
+   const std::string inputFileName = "input";
+   std::ifstream inputFile(inputFileName);
+   if ( inputFile.is_open() ) {
+      while ( inputFile ) {
+         std::getline(inputFile, currentLine);
+         int lineLength = currentLine.length();
+
+         int currentValue = 0;
+         // find first number
+         for(int i=0; i<lineLength; i++)
+         {
+            const char currentChar = currentLine[i];
+            if(currentChar>='0' && currentChar<='9')
+            {
+//         std::cout << currentChar << " " << currentChar-'0' << " ";
+               currentValue = 10 * (currentChar-'0');
+               break;
+            }
+         }
+
+         // find last number
+         for(int i=lineLength-1; i>=0; i--)
+         {
+            const char currentChar = currentLine[i];
+            if(currentChar>='0' && currentChar<='9')
+            {
+//         std::cout << currentChar << " " << currentChar-'0' << " - ";
+               currentValue += (currentChar-'0');
+               break;
+            }
+         }
+
+         std::cout << currentValue << " - " << currentLine << std::endl;
+
+         totalSum += currentValue;
+      }
+   }
+
+   std::cout << "Total: " << totalSum << std::endl;
 }
