10fe75df944d7c3f604879908a361e10c812ef98
Matias Lopez <matias.lopez@rocketinsights.com>
2023-12-18 15:47:56 -0300

    Day04 complete

--- Day04/day04.cpp
+++ Day04/day04.cpp
@@ -83,6 +83,85 @@ public:
    void calculateSecondPuzzleAnswer()
    {
       this->_secondPuzzleAnswer = 0;
+
+      // save number of matching numbers per line
+      int matches[_fileInput.size()];
+      for(int i=0; i<_fileInput.size(); ++i)
+      {
+         std::string line = _fileInput[i];
+         int lineLength = line.length();
+
+         // parse game id
+//         std::string::size_type parsePos = line.find(":");
+//         int gameId = atoi(line.substr(5, parsePos).c_str());
+//         std::cout << line.substr(0, WINNING_NUMBERS_POSITION);
+
+         // discriminate winning numbers
+         std::vector<int> winningNumbers;
+         int pos = WINNING_NUMBERS_POSITION;
+         for(int j=0; j<WINNING_NUMBERS_SIZE; ++j)
+         {
+            std::string nString = line.substr(pos, NUMBERS_WIDTH+1);
+//            std::cout << "- " << nString;
+            winningNumbers.push_back(atoi(nString.c_str()));
+//            std::cout << winningNumbers[j] << " # ";
+            pos += NUMBERS_WIDTH+1;
+         }
+//      std::cout << std::endl;
+
+         // discriminate your numbers
+         std::vector<int> yourNumbers;
+         pos = YOUR_NUMBERS_POSITION;
+         for(int j=0; j<YOUR_NUMBERS_SIZE; ++j)
+         {
+            std::string nString = line.substr(pos, NUMBERS_WIDTH+1);
+//            std::cout << "- " << nString;
+            yourNumbers.push_back(atoi(nString.c_str()));
+//            std::cout << yourNumbers[j] << " # ";
+            pos += NUMBERS_WIDTH+1;
+         }
+
+         // verify points
+         matches[i] = 0;
+         for(int j=0; j<WINNING_NUMBERS_SIZE; ++j)
+         {
+            for(int k=0; k<YOUR_NUMBERS_SIZE; ++k)
+            {
+               if( winningNumbers[j] == yourNumbers[k] )
+               {
+//            std::cout << "- " << winningNumbers[j];
+                  matches[i]++;
+               }
+            }
+         }
+      }
+
+      // calculate number of copies
+      int copies[_fileInput.size()];
+      for(int i=0; i<_fileInput.size(); ++i)
+      {
+         copies[i] = 1;
+      }
+      for(int i=0; i<_fileInput.size(); ++i)
+      {
+         for(int j=1; j<=matches[i]; ++j)
+         {
+            copies[i+j] += copies[i];
+         }
+      }
+
+      // multiply points per copies
+      for(int i=0; i<_fileInput.size(); ++i)
+      {
+         this->_secondPuzzleAnswer += copies[i];
+            std::cout << i << " - " << copies[i] << " " << matches[i] << " > " << this->_secondPuzzleAnswer << std::endl;
+//         if( matches[i] > 0 )
+//         {
+//            int points = (matches[i] == 1) ? 1 : pow(2,matches[i]-1);
+//            std::cout << i << " - " << matches[i] << " " << points << " " << copies[i] << " = " << (points * copies[i]) << std::endl;
+//            this->_secondPuzzleAnswer += (points * copies[i]);
+//         }
+      }
    }
 
    void calculateAnswers(std::string inputFileName)
